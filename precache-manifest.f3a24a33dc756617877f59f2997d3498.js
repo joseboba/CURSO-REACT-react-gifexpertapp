46dee202f68148fe5c154f07f30feb0cda677fc9
Jose Enrique <joseboba18@gmail.com>
2020-08-12 17:44:11 -0600

    Updates

---
+++ precache-manifest.f3a24a33dc756617877f59f2997d3498.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33e36e7a9053073db949381275189a0a",
+    "url": "/react-gifexpertapp/index.html"
+  },
+  {
+    "revision": "fce5d053322cf723b788",
+    "url": "/react-gifexpertapp/static/css/main.dc738b7a.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/react-gifexpertapp/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gifexpertapp/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fce5d053322cf723b788",
+    "url": "/react-gifexpertapp/static/js/main.d40a1966.chunk.js"
+  },
+  {
+    "revision": "8d60da8017703a69006d",
+    "url": "/react-gifexpertapp/static/js/runtime-main.ae19a04c.js"
+  }
+]);
